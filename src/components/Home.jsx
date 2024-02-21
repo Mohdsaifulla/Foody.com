77f8dcf6ba0162ccbe6d80d7fda7ad7ce91347bc
Mohdsaifulla <saif49032@gmail.com>
2024-02-21 12:15:28 +0530

    changes in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { fetchedApi } from "../fetchedApi/fetchedApi";
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetchData();
+  });
+  const fetchData = async () => {
+    try {
+      const data = await fetchedApi();
+      console.log("Fetched data:", data);
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
+    }
+    console.log(data) 
+  };
+  return <div>Home</div>;
+};
 
-export default Home
+export default Home;
---
+++ src/fetchedApi/fetchedApi.js
@@ -0,0 +1,7 @@
+export const fetchedApi = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.46210&lng=78.57580&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const jsonData = await data.json();
+    return jsonData;
+  };
