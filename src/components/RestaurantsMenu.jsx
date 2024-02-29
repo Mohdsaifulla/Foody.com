c664f6a189786cf08522388797fa4d1aee2f320a
Mohdsaifulla <saif49032@gmail.com>
2024-02-29 20:36:41 +0530

    fix-error-in-menu

--- src/components/RestaurantsMenu.jsx
+++ src/components/RestaurantsMenu.jsx
@@ -1,29 +1,25 @@
 import React, { useEffect, useState } from "react";
-import  {MENU_API} from "../constants/constants"
+// import { MENU_API } from "../constants/constants";
 import { useParams } from "react-router-dom";
 const RestaurantsMenu = () => {
   const [menu, setMenu] = useState([]);
-const {restaurantId}=useParams()
-console.log(restaurantId)
+  const { restaurantId } = useParams();
+  // console.log(restaurantId);
   const fetchRestMenu = async () => {
-
+    //`${MENU_API}${restaurantId}`
     const data = await fetch(
-     `${MENU_API}${restaurantId}`
+      `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5355161&lng=77.3910265&restaurantId=${restaurantId}&catalog_qa=undefined&submitAction=ENTER`
     );
     const jsonData = await data.json();
-    console.log(jsonData?.data);
-    setMenu(jsonData?.data.cards[0].card.card.text)
+    console.log(jsonData?.data.cards[0].card.card);
+    setMenu(jsonData?.data.cards[0].card.card.info.name);
   };
   useEffect(() => {
-fetchRestMenu()
+    fetchRestMenu();
   }, []);
 
   console.log(menu);
-  return (
-    <div>
-    {menu}
-    </div>
-  );
+  return <div>{menu}</div>;
 };
 
 export default RestaurantsMenu;
--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -11,7 +11,12 @@ export const navLinks = [
   { id: 5, name: "Cart", icon: <LuShoppingCart />, to: "/cart" },
 ];
 
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
 
-export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/"
+// export const MENU_API =
+//   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5355161&lng=77.3910265&restaurantId=355000&catalog_qa=undefined&submitAction=ENTER";
 
-export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5355161&lng=77.3910265&restaurantId="
+// export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5355161&lng=77.3910265&restaurantId="
+
+// https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5355161&lng=77.3910265&restaurantId=355000&catalog_qa=undefined&submitAction=ENTER
